b907a5ebdbfe5aeb3b86e067f44d2b91270e5cad
Carlos Bello <72536521+carlostbello@users.noreply.github.com>
2022-11-16 16:00:56 -0800

    Update extract.py

---
+++ src/ecotope_package_cs2306/extract.py
@@ -0,0 +1,18 @@
+from typing import List
+import pandas as pd
+from ftplib import FTP
+from datetime import datetime
+def get_noaa_data(stations: List[str]):
+    year_end = datetime.today().year
+    for year in range(2010, year_end+1):
+        hostname = f"ftp.ncdc.noaa.gov"
+        wd = f"/pub/data/noaa/isd-lite/{year}/"
+        ftp_server = FTP(hostname)
+        ftp_server.login()
+        ftp_server.cwd(wd)
+        ftp_server.encoding = "utf-8"
+        for station in stations:
+            filename = f"{station}-{year}.gz"
+            with open(filename, "wb") as file:
+                ftp_server.retrbinary(f"RETR {filename}", file.write)
+
