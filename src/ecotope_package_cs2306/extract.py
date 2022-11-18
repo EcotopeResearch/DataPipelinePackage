583ced350ec8d08da38e169201928466e0a7bb02
Carlos Bello <72536521+carlostbello@users.noreply.github.com>
2022-11-18 01:14:30 -0800

    completed the fetching of the station ids given station

--- src/ecotope_package_cs2306/extract.py
+++ src/ecotope_package_cs2306/extract.py
@@ -3,13 +3,37 @@ import pandas as pd
 from ftplib import FTP
 from datetime import datetime
 import gzip
+import os
 
-def get_noaa_data(stations: List[str]):
-    noaa_filenames = download_noaa_data(stations)
+def get_noaa_data(station_names: List[str]):
+    noaa_dictionary = get_noaa_dictionary()
+    station_ids = [noaa_dictionary[station_name][0] for station_name in station_names if station_name in noaa_dictionary]
+    print(station_ids)
+    noaa_filenames = download_noaa_data(station_ids)
+    print(noaa_filenames)
     noaa_dfs = convert_to_df(noaa_filenames)
+    print(noaa_dfs)
+
+def get_noaa_dictionary() -> dict:
+    filename = "isd-history.csv"
+    hostname = f"ftp.ncdc.noaa.gov"
+    wd = f"/pub/data/noaa/"
+    ftp_server = FTP(hostname)
+    ftp_server.login()
+    ftp_server.cwd(wd)
+    ftp_server.encoding = "utf-8"
+    with open(f"output/{filename}", "wb") as file:
+        ftp_server.retrbinary(f"RETR {filename}", file.write)
+    ftp_server.quit()
+    isd_history = pd.read_csv(f"output/isd-history.csv",dtype=str)
+    isd_history["USAF_WBAN"] = isd_history['USAF'].str.cat(
+        isd_history['WBAN'], sep ="-")
+    df_id_usafwban = isd_history[["ICAO", "USAF_WBAN"]]
+    df_id_usafwban = df_id_usafwban.drop_duplicates(subset = ["ICAO"], keep = 'first')
+    return df_id_usafwban.set_index("ICAO").T.to_dict('list')
 
 def download_noaa_data(stations: List[str]) -> List[str]:
-    noaa_filenames = List()
+    noaa_filenames = list()
     year_end = datetime.today().year
     for year in range(2010, year_end+1):
         hostname = f"ftp.ncdc.noaa.gov"
@@ -21,19 +45,21 @@ def download_noaa_data(stations: List[str]) -> List[str]:
         for station in stations:
             filename = f"{station}-{year}.gz"
             noaa_filenames.append(filename)
-            with open(f"output/{filename}", "wb") as file:
-                ftp_server.retrbinary(f"RETR {filename}", file.write)
-                ftp_server.quit()
-                #STOP
+            file_path = f"output/{filename}"
+            if os.path.exists(file_path) == False:
+                with open(file_path, "wb") as file:
+                    ftp_server.retrbinary(f"RETR {filename}", file.write)
+            else:
+                print(file_path, " exists")
         ftp_server.quit()
     return noaa_filenames
 
-def convert_to_df(noaa_filenames: List[str]) -> List[pd.DataFrame]:
-    noaa_dfs = List()
+def convert_to_df(noaa_filenames: List[str]) -> dict:
+    noaa_dfs = []
     for filename in noaa_filenames:
         noaa_dfs.append(gz_to_df(filename))
-    return noaa_dfs
-        
+    noaa_dfs_dict = dict(zip(noaa_filenames, noaa_dfs))
+    return noaa_dfs_dict
 
 def gz_to_df(filename: str) -> pd.DataFrame:
     with gzip.open(f"output/{filename}") as data:
@@ -42,6 +68,7 @@ def gz_to_df(filename: str) -> pd.DataFrame:
 
 def main():
     stations = ["727935-24234"]
-    #get_noaa_data(stations)
+    #, 'KPWM', 'KSFO', 'KAVL'
+    get_noaa_data(['KBFI'])
 
 main()
