e8e8dd142e44a54db2fd6221a658a4a960e80c4b
Carlos Bello <72536521+carlostbello@users.noreply.github.com>
2022-11-22 11:37:50 -0800

    Added more helper functions to fetch NOAA FTP data

--- src/ecotope_package_cs2306/extract.py
+++ src/ecotope_package_cs2306/extract.py
@@ -4,17 +4,27 @@ from ftplib import FTP
 from datetime import datetime
 import gzip
 import os
+import re
 
-def get_noaa_data(station_names: List[str]):
-    noaa_dictionary = get_noaa_dictionary()
+def get_noaa_data(station_names: List[str]) -> dict:
+    noaa_dictionary = _get_noaa_dictionary()
     station_ids = [noaa_dictionary[station_name][0] for station_name in station_names if station_name in noaa_dictionary]
-    print(station_ids)
-    noaa_filenames = download_noaa_data(station_ids)
-    print(noaa_filenames)
-    noaa_dfs = convert_to_df(noaa_filenames)
+    noaa_filenames = _download_noaa_data(station_ids)
+    noaa_dfs = _convert_to_df(noaa_filenames)
     print(noaa_dfs)
 
-def get_noaa_dictionary() -> dict:
+def _format_df(station_ids: dict, noaa_dfs: dict):
+    formatted_dfs = {}
+    for key1, value1 in station_ids:
+
+        temp_df = pd.DataFrame()
+        for key, value in noaa_dfs.items():
+            if key.startswith(value1):
+                temp_df.append(value)
+
+
+
+def _get_noaa_dictionary() -> dict:
     filename = "isd-history.csv"
     hostname = f"ftp.ncdc.noaa.gov"
     wd = f"/pub/data/noaa/"
@@ -32,20 +42,24 @@ def get_noaa_dictionary() -> dict:
     df_id_usafwban = df_id_usafwban.drop_duplicates(subset = ["ICAO"], keep = 'first')
     return df_id_usafwban.set_index("ICAO").T.to_dict('list')
 
-def download_noaa_data(stations: List[str]) -> List[str]:
+def _download_noaa_data(stations: List[str]) -> List[str]:
     noaa_filenames = list()
     year_end = datetime.today().year
+    # Download files for each station from 2010 till present year
     for year in range(2010, year_end+1):
+        # Set FTP credentials and connect
         hostname = f"ftp.ncdc.noaa.gov"
         wd = f"/pub/data/noaa/isd-lite/{year}/"
         ftp_server = FTP(hostname)
         ftp_server.login()
         ftp_server.cwd(wd)
         ftp_server.encoding = "utf-8"
+        # Download all files and save as station_year.gz in /output
         for station in stations:
             filename = f"{station}-{year}.gz"
             noaa_filenames.append(filename)
             file_path = f"output/{filename}"
+            # Do not download if the file already exists
             if os.path.exists(file_path) == False:
                 with open(file_path, "wb") as file:
                     ftp_server.retrbinary(f"RETR {filename}", file.write)
@@ -54,14 +68,17 @@ def download_noaa_data(stations: List[str]) -> List[str]:
         ftp_server.quit()
     return noaa_filenames
 
-def convert_to_df(noaa_filenames: List[str]) -> dict:
+def _convert_to_df(noaa_filenames: List[str]) -> dict:
+    # Gets the list of downloaded filenames and imports the files
+    # and converts it to a dictionary of DataFrames
     noaa_dfs = []
     for filename in noaa_filenames:
-        noaa_dfs.append(gz_to_df(filename))
+        noaa_dfs.append(_gz_to_df(filename))
     noaa_dfs_dict = dict(zip(noaa_filenames, noaa_dfs))
     return noaa_dfs_dict
 
-def gz_to_df(filename: str) -> pd.DataFrame:
+def _gz_to_df(filename: str) -> pd.DataFrame:
+    # Opens the file and returns it as a pd.DataFrame
     with gzip.open(f"output/{filename}") as data:
         table = pd.read_table(data, header=None)
     return table
