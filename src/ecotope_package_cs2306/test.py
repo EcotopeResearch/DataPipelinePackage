4e7fc1a68418786f1f80fde015bae6c52f0947bb
Julian Harris <harrisjulian@seattleu.edu>
2023-01-10 13:49:58 -0800

    testing extract and load

---
+++ src/ecotope_package_cs2306/test.py
@@ -0,0 +1,17 @@
+import extract
+import load
+
+def __main__():
+    """
+    stations = ["727935-24234"]
+    #, 'KPWM', 'KSFO', 'KAVL'
+    formatted_dfs = extract.get_noaa_data(['KBFI'])
+    print(formatted_dfs)
+    for key, value in formatted_dfs.items():
+        value.to_csv(f"output/{key}.csv", index=False)
+        print("done 1")
+    print("done")
+    """
+
+if __name__ == '__main__':
+    __main__()
