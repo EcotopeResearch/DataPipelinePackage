1ff68b837407da09e6001b41cda8eb5857f4c3ce
Julian Harris <harrisjulian@seattleu.edu>
2023-01-14 17:07:33 -0800

    Updated transform.py, added test functions

--- src/ecotope_package_cs2306/transform.py
+++ src/ecotope_package_cs2306/transform.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import numpy as np
 
-#required input files
+#input files for tests, will come from parameters come deployment
 vars_filename = "input/Variable_Names.csv" #currently set to a test until real csv is completed
 
-#STRETCH GOAL
+
+#TODO: _removeOutliers STRETCH GOAL
 #Functionality for alarms being raised based on bounds needs to happen here. 
-#Try and keep things clean so that can be added later. 
-def _removeOutliers(df, vars_filename):
+def _removeOutliers(df : pd.DataFrame, vars_filename) -> pd.DataFrame:
     """
-    Function will take the location of extracted data in a json and bounds information in a csv,
-    store the json data in a pandas dataframe, then remove outliers above or below bounds as 
+    Function will take a pandas dataframe and location of bounds information in a csv,
+    store the bounds data in a dataframe, then remove outliers above or below bounds as 
     designated by the csv. Function then returns the resulting dataframe. 
     Input: Pandas dataframe and file location of variable processing information
     Output: Pandas dataframe 
@@ -21,7 +21,6 @@ def _removeOutliers(df, vars_filename):
     bounds_df.dropna(axis=0, thresh=2, inplace=True)
     bounds_df.set_index(['variable_name'], inplace=True)
     bounds_df = bounds_df[bounds_df.index.notnull()]
-    
     #bad data removal loop
     for columnVar in df:
         if(columnVar in bounds_df.index):
@@ -40,20 +39,25 @@ def _removeOutliers(df, vars_filename):
     return df
 
 
-#What are the select variables? Just variables with numeric inputs?
-def _fillMissing(df):
+def _fillMissing(df : pd.DataFrame, vars_filename) -> pd.DataFrame:
     """
     Function will take a pandas dataframe and forward fill select variables with no entry. 
     Input: Pandas dataframe
     Output: Pandas dataframe
     """
+    #ffill dataframe holds ffill length and changepoint bool
+    ffill_df = pd.read_csv(vars_filename)
+    ffill_df = ffill_df.loc[:, ["variable_name", "changepoint", "ffill_length"]]
+    ffill_df.dropna(axis=0, thresh=2, inplace=True) #drop data without changepoint AND ffill_length
+    ffill_df.set_index(['variable_name'], inplace=True)
+    ffill_df = ffill_df[ffill_df.index.notnull()]
 
     #ONLY forward fill if cumulative sum of var is not zero, e.g. don't do it until you find at least one valid entry first.
     #ffill_length column specifies time in minutes non-changepoint vars may be forward filled if missing. 
     #vars with changepoint column set to 1, forward fill without restriction. obv cumulative sum still applies.
 
-    #forward fill being take previous var value and insert it. we need to do this column by column.
-    #probably use pd.ffill
+    #TODO: Forward filling
+    #df.ffill
 
     return df
 
@@ -84,21 +88,29 @@ def sensor_adjustment(df : pd.DataFrame) -> pd.DataFrame:
 
 
 #Test function for simple main, will be removed once transform.py is complete
-def outlier_fillTest():
-    #Sample df, this should come from extract in actual running
+def outlierTest():
     testdf_filename = "input/ecotope_wide_data.csv"
     df = pd.read_csv(testdf_filename)
 
     print("\nTesting _removeOutliers...\n")
     print(_removeOutliers(df, vars_filename))
     print("\nFinished testing _removeOutliers\n")
+#Test function for simple main, will be removed once transform.py is complete
+def ffillTest():
+    testdf_filename = "input/ecotope_wide_data.csv"
+    df = pd.read_csv(testdf_filename)
 
-    #print("\nTesting _fillMissing...\n")
+    print("\nTesting _fillMissing...\n")
+    _fillMissing(df, vars_filename)
+    print("\nFinished testing _fillMissing\n")
 
 
 #Test main, will be removed once transform.py is complete
 def __main__():
-    outlier_fillTest()
+    #outlierTest()
+    ffillTest()
+    pass
+
 
 
 if __name__ == '__main__':
