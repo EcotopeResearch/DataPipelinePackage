52114c30dea8baebcc4472757bdef7818f39f95e
Carlos Bello <bellocarlos@seattleu.edu>
2023-01-24 00:01:42 -0800

    Update __init__.py

--- src/ecotope_package_cs2306/__init__.py
+++ src/ecotope_package_cs2306/__init__.py
@@ -4,5 +4,5 @@ from.load import getLoginInfo, connectDB, checkTableExists, createNewTable, crea
 
 #from .load import getLoginInfo, connectDB, checkTableExists, createNewTable, createUnknownColumns, loadDatabase
 
-__all__ = ["get_noaa_data", "merge_noaa", "json_to_df", "extract_files", "remove_outliers", "ffill_missing", "sensor_adjustment", "get_energy_by_min", "verify_power_energy",
+__all__ = ["get_noaa_data", "json_to_df", "extract_files", "remove_outliers", "ffill_missing", "sensor_adjustment", "get_energy_by_min", "verify_power_energy",
            "aggregate_values", "calculate_cop_values", "getLoginInfo", "connectDB", "checkTableExists", "createNewTable", "createUnknownColumns", "loadDatabase", "rename_sensors"]
