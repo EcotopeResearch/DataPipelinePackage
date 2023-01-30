2765896fee8d40a3904fd798064da8ff7927bdd7
Carlos Bello <72536521+carlostbello@users.noreply.github.com>
2023-01-30 11:53:47 -0800

    Removed comment from init

--- src/ecotope_package_cs2306/__init__.py
+++ src/ecotope_package_cs2306/__init__.py
@@ -2,7 +2,5 @@ from .extract import set_input, set_output, get_noaa_data, json_to_df, extract_f
 from .transform import rename_sensors, avg_duplicate_times, remove_outliers, ffill_missing, sensor_adjustment, get_energy_by_min, verify_power_energy, aggregate_values, calculate_cop_values
 from.load import getLoginInfo, connectDB, checkTableExists, createNewTable, createUnknownColumns, loadDatabase
 
-#from .load import getLoginInfo, connectDB, checkTableExists, createNewTable, createUnknownColumns, loadDatabase
-
 __all__ = ["get_last_line", "set_config", "set_input", "set_output","get_noaa_data", "json_to_df", "extract_files", "avg_duplicate_times", "remove_outliers", "ffill_missing", "sensor_adjustment", "get_energy_by_min", "verify_power_energy",
            "aggregate_values", "calculate_cop_values", "getLoginInfo", "connectDB", "checkTableExists", "createNewTable", "createUnknownColumns", "loadDatabase", "rename_sensors"]
