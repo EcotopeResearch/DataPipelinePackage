a62d31dcad8636add5d09ec2c986022b269f1e6e
Carlos Bello <72536521+carlostbello@users.noreply.github.com>
2023-02-03 14:17:29 -0800

    Testing changes for config

--- src/ecotope_package_cs2306/config.py
+++ src/ecotope_package_cs2306/config.py
@@ -1,50 +1,49 @@
 # Sets configurations
 # TODO This doesn't work
-def set_input(input: str = "input/"):
+
+_input_directory = "input/"
+_output_directory = "output/"
+_data_directory = "data/"
+_config_directory = "input/config.ini"
+
+def set_input(input: str):
     """
     Accessor function to set input directory in the format {directory}/
     Defaults to input/
     Input: String of relative directory
     """
+    global _input_directory
     _input_directory = input
     return _input_directory
 
 
-def set_output(output: str = "output/"):
+def set_output(output: str):
     """
     Accessor function to set output directory in the format {directory}/
     Defaults to output/
     Input: String of relative directory
     """
+    global _output_directory
     _output_directory = output
     return _output_directory
 
 
-def set_data(data: str = "data/"):
+def set_data(data: str):
     """
     Accessor function to set data directory in the format {directory}/
     Defaults to data/
     Input: String of relative directory
     """
+    global _data_directory
     _data_directory = data
     return _data_directory
 
-def set_config(cfg: str = "input/config.ini"):
+def set_config(cfg: str):
     """
     Accessor function to set input directory in the format {directory}/
     Defaults to input/
     Input: String of relative directory
     """
+    global _config_directory
     _config_directory = cfg
     return _config_directory
-
-
-global _input_directory
-global _output_directory
-global _data_directory
-global _config_directory
-
-_input_directory = set_input()
-_output_directory = set_output()
-_data_directory = set_data()
-_config_directory = set_config()
