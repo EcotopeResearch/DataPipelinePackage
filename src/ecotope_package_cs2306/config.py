d0c8e973f87597d78f772be66edd16574bb9288d
Carlos Bello <bellocarlos@seattleu.edu>
2023-02-07 13:08:42 -0800

    Updated config

--- src/ecotope_package_cs2306/config.py
+++ src/ecotope_package_cs2306/config.py
@@ -1,49 +1,18 @@
 # Sets configurations
 # TODO This doesn't work
+import configparser
+import os
+import sys
 
-_input_directory = "input/"
-_output_directory = "output/"
-_data_directory = "data/"
-_config_directory = "input/config.ini"
+_config_directory = "config.ini"
 
-def set_input(input: str):
-    """
-    Accessor function to set input directory in the format {directory}/
-    Defaults to input/
-    Input: String of relative directory
-    """
-    global _input_directory
-    _input_directory = input
-    return _input_directory
+if not os.path.exists(_config_directory):
+    print(f"File path '{_config_directory}' does not exist.")
+    sys.exit()
 
+configure = configparser.ConfigParser()
+configure.read(_config_directory)
 
-def set_output(output: str):
-    """
-    Accessor function to set output directory in the format {directory}/
-    Defaults to output/
-    Input: String of relative directory
-    """
-    global _output_directory
-    _output_directory = output
-    return _output_directory
-
-
-def set_data(data: str):
-    """
-    Accessor function to set data directory in the format {directory}/
-    Defaults to data/
-    Input: String of relative directory
-    """
-    global _data_directory
-    _data_directory = data
-    return _data_directory
-
-def set_config(cfg: str):
-    """
-    Accessor function to set input directory in the format {directory}/
-    Defaults to input/
-    Input: String of relative directory
-    """
-    global _config_directory
-    _config_directory = cfg
-    return _config_directory
+_input_directory = configure.get('input', 'directory')
+_output_directory = configure.get('output', 'directory')
+_data_directory = configure.get('data', 'directory')
