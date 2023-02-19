a373bc6d460d2c6ac6a386b1099f6301335869fc
Julian Harris <harrisjulian@seattleu.edu>
2023-02-18 18:46:05 -0800

    load_test.py structure updated, working on proper import/simulation of load functionality.

--- testing/Bayview/load/load_test.py
+++ testing/Bayview/load/load_test.py
@@ -1,3 +1,4 @@
+#NOTE: I import everything load.py does, but if I'm just importing that to use as a package, not necessary?
 import configparser
 import mysql.connector
 from mysql.connector import errorcode
@@ -13,16 +14,43 @@ from ecotope_package_cs2306.config import _config_directory
 Unit testing for each function in load, meant to test all 
 plausible cases for each function to ensure scripts are 
 working as intended. 
+
+GENERAL UNIT TEST PHILOSOPHY FOR EACH FUNCTION: 
+Make a list of all features a function should have
+Write a test that ensures that feature is working as intended (for all plausible cases)
 """
 
-#UNITTEST: getLoginInfo -- ImportDocuments and have test here!!
-def UNIT_TEST_getLoginInfo(table_headers: list, config_info : str = _config_directory) -> dict:
-    """
-    Function will and return config.ini in a config var.
-    Output: Login information
-    """
-    return
+#NOTE: I need to import load.py similar to how it will be done in bayview, how should I go about that?
 
-#UNITTEST: connectDB
-def UNIT_TEST_connectDB():
-    return
+#inherits from TestCase class to add our own tests
+class Test_Load(ut.TestCase):
+        
+    #UNITTEST: getLoginInfo
+    def test_getLoginInfo(self):
+        return
+
+    #UNITTEST: connectDB
+    def test_connectDB(self):
+        return
+
+    #UNITTEST: checkTableExists
+    def test_checkTableExists(self):
+        return
+
+    #UNITTEST: createNewTable
+    def test_createNewTable(self):
+        return
+
+    #UNITTEST: loadDatabase
+    def test_loadDatabase(self):
+        return
+
+    def test_fail(self):
+        self.assertEqual(3, 5)
+    def test_pass(self):
+        self.assertEqual(5, 5)
+
+
+if __name__ == '__main__':
+    #runs test_xxx functions, shows what passed or failed. 
+    ut.main()
