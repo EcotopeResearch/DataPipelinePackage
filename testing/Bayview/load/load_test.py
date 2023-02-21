26484ca392c1d5dc9f5384c3213a7063d5eec286
Julian Harris <harrisjulian@seattleu.edu>
2023-02-20 16:49:28 -0800

    Updated load_test.py, added tests for table functions, planned loadDatabase function tests.

--- testing/Bayview/load/load_test.py
+++ testing/Bayview/load/load_test.py
@@ -35,6 +35,8 @@ class Test_Load(unittest.TestCase):
             "database" : "testDB"
         }
     table_list = ["minute", "hour", "day"]
+    #need a list of all the columns, unfortunately that list is HUGE
+    column_list = []
 
     #TEST FIXTURES BELOW
     #Fixture being prep needed to perform tests, such as running a database, directories, or cleanup.
@@ -55,6 +57,7 @@ class Test_Load(unittest.TestCase):
         #this is how it's setup in bayview load? I don't know how else to call it?
         config_dict = getLoginInfo(self.table_list)
 
+        #currently, this fails, despite it being called exactly how it is in bayview load
         test_login_dict = config_dict['database']
 
         #test makes sure data was loaded correctly
@@ -72,22 +75,49 @@ class Test_Load(unittest.TestCase):
         db_cursor.close    
 
     #UNITTEST: checkTableExists
+    #NOTE: Helper function in bayview run
     def test_checkTableExists(self):
+        #NOTE: I NEED a cursor to test this, so connectDB needs to be working for this to work
+        db_connection, db_cursor = connectDB(self.login_dict)
 
-        return
+        #Bool should be true #MAKE SURE IT'S A TABLE THAT'S ALWAYS THERE!!
+        trueBool = checkTableExists(db_cursor, "test_table_DON'T_REMOVE") 
+        #Bool should be false
+        falseBool = checkTableExists(db_cursor, "this_table_doesn't_exist")
+
+        self.assertEqual(trueBool, True)
+        self.assertEqual(falseBool, False)
 
     #UNITTEST: createNewTable
+    #NOTE: Helper function in bayview run
     def test_createNewTable(self):
-        return
+        #NOTE: I NEED a cursor for this, so connect_DB must be working for this test to pass
+        db_connection, db_cursor = connectDB(self.login_dict)
+
+        #test by creating a table called "test_table_DON'T_REMOVE"
+        #run a second time to make sure that it works w/the table already being created? or is the other check for that?
+
+        #try creating a table that doesn't exist yet, a normal test
+        trueBool = createNewTable(db_cursor, "new_table", ["col1", "col2", "col3"])
+
+        #try creating a table that exists, error checkingx
+        falseBool = createNewTable(db_cursor, self.table_list[0], self.column_list)
+
+        self.assertEqual(trueBool, True)
+        self.assertEqual(falseBool, False)
 
     #UNITTEST: loadDatabase
     def test_loadDatabase(self):
-
         #how can I look at a database and assert that it has stuff? 
         #I'll prolly copy what is done in load to check if there's already stuff?
         #mysql.connector is imported.
+        db_connection, db_cursor = connectDB(self.login_dict)
 
         #NOTE: I need a pickle w/processed bayview data, just after extract. 
+        #NOTE: I'm going to wait until I can work with someone more familiar with load and such to make this.
+
+        #The basic idea will be to fetch the # of rows in the tables of minute, hourly, and daily, and make sure they
+        #match with what they should be. If that's all good, we call that a successful test. 
 
         return
     
