2c7a6a663de4d81bb5da7c086dcc30a49e2b6197
Emil Fahrig <emilx@outlook.com>
2023-02-26 16:59:25 -0800

    finished load unit testing... for now :)

--- testing/Bayview/load/load_test.py
+++ testing/Bayview/load/load_test.py
@@ -32,16 +32,19 @@ class Test_Load(unittest.TestCase):
     login_dict = {'database': {'user': 'fahrigemil', 'password': 'aBC12345!', 'host': 'csp70.cslab.seattleu.edu', 'database': 'testDB'},
                   'day': {'table_name': 'bayview_day', 
                              'sensor_list': ['sensor1', 'sensor2', 'sensor3', 'sensor4', 'sensor5']}}
-    incorrect_password_login_dict = {'database': {'user': 'fahrigemil', 'password': 'wrong', 'host': 'csp70.cslab.seattleu.edu', 'database': 'testDB'}}
+    incorrect_login_dict = {'database': {'user': 'fahrigemil', 'password': 'wrong', 'host': 'csp70.cslab.seattleu.edu', 'database': 'testDB'}}
     test_headers = ["day"]
     test_config_path = "test_config.ini"
     load_data = pd.DataFrame(np.random.randint(1, 10, size=(5, 5)).astype(float), columns=login_dict["day"]["sensor_list"], 
                              index=[datetime(2022, 1, i) for i in range(1, 6)])
     new_data = pd.DataFrame(np.random.randint(1, 10, size=(3, 5)).astype(float), columns=login_dict["day"]["sensor_list"], 
                     index=[datetime(2022, 1, i) for i in range(6, 9)])
+    bad_columns_data = pd.DataFrame(np.random.randint(1, 10, size=(3, 5)).astype(float), columns=["bad1", "bad2", "bad3", "bad4", "bad5"], 
+                    index=[datetime(2022, 1, i) for i in range(6, 9)])
+    bad_dtype_data = pd.DataFrame(np.random.randint(1, 10, size=(3, 5)).astype(float), columns=login_dict["day"]["sensor_list"], 
+                    index=[datetime(2022, 1, i) for i in range(6, 9)])
+    bad_dtype_data.at["2022-01-06", "sensor2"] = "bad"
     
-
-    """
     def test_correctheader_getLoginInfo(self):
         config_dict = getLoginInfo(self.test_headers, self.test_config_path)
         self.assertDictEqual(config_dict, self.login_dict)
@@ -50,7 +53,7 @@ class Test_Load(unittest.TestCase):
         self.assertRaises(configparser.NoSectionError, getLoginInfo, ["bad"], self.test_config_path)
         
     def test_incorrectpassword_connectDB(self):
-        cxn, cursor = connectDB(self.incorrect_password_login_dict)
+        cxn, cursor = connectDB(self.incorrect_login_dict)
         self.assertEqual(cxn, None)
         self.assertEqual(cursor, None)
 
@@ -79,9 +82,9 @@ class Test_Load(unittest.TestCase):
 
         cursor.execute("drop table bayview_day")
 
+        cxn.commit()
         cxn.close()
         cursor.close()
-    """
 
     def test_existing_table_loadDatabase(self):
         cxn, cursor = connectDB(self.login_dict["database"])
@@ -105,82 +108,32 @@ class Test_Load(unittest.TestCase):
 
         cursor.execute("drop table bayview_day")
 
+        cxn.commit()
+        cxn.close()
+        cursor.close()
+    
+    def test_bad_dataFrame_loadDatabase(self):
+        cxn, cursor = connectDB(self.login_dict["database"])
+        self.assertRaises(mysql.connector.errors.ProgrammingError, loadDatabase, cursor, self.bad_columns_data, self.login_dict, "day")
+        cursor.execute("drop table bayview_day")
+
+        cxn.commit()
         cxn.close()
         cursor.close()
 
+    def test_bad_type_loadDatabase(self):
+        cxn, cursor = connectDB(self.login_dict["database"])
+        self.assertRaises(KeyError, loadDatabase, cursor, self.load_data, self.login_dict, "bad")
 
-    
+        cxn.close()
+        cursor.close()
 
-    """
-    #UNITTEST: connectDB
-    def test_connectDB(self):
-        #when this is successful, it prints "Successfully connected to database."
-        db_connection, db_cursor = connectDB(self.login_dict)
-        #NOTE: Test behavior when wrong login credentials passed in 
+    def test_bad_data_loadDatabase(self):
+        cxn, cursor = connectDB(self.login_dict["database"])
+        self.assertRaises(mysql.connector.errors.DatabaseError, loadDatabase, cursor, self.bad_dtype_data, self.login_dict, "day")
 
-        #how can I assert connections/test that it's valid? Is there a way for a cursor or connection object to return false?
-
-        db_connection.close
-        db_cursor.close    
-    """
-    """
-    #UNITTEST: checkTableExists
-    #NOTE: Helper function in bayview run
-    def test_checkTableExists(self):
-        #NOTE: I NEED a cursor to test this, so connectDB needs to be working for this to work
-        db_connection, db_cursor = connectDB(self.login_dict)
-
-        #Bool should be true #MAKE SURE IT'S A TABLE THAT'S ALWAYS THERE!!
-        trueBool = checkTableExists(db_cursor, "existing_table") 
-        #Bool should be false
-        falseBool = checkTableExists(db_cursor, "this_table_doesn't_exist")
-
-        self.assertEqual(trueBool, True)
-        self.assertEqual(falseBool, False)
-
-        db_connection.close
-        db_cursor.close
-
-    #UNITTEST: createNewTable
-    #NOTE: Helper function in bayview run
-    def test_createNewTable(self):
-        #NOTE: I NEED a cursor for this, so connect_DB must be working for this test to pass
-        db_connection, db_cursor = connectDB(self.login_dict)
-
-        #test by creating a table called "test_table_DON'T_REMOVE"
-        #run a second time to make sure that it works w/the table already being created? or is the other check for that?
-
-        #try creating a table that doesn't exist yet, a normal test
-        trueBool = createNewTable(db_cursor, "new_table", ["col1", "col2", "col3"])
-
-        #try creating a table that exists, error checking
-        falseBool = createNewTable(db_cursor, "existing_table", ["col1", "col2", "col3"])
-
-        self.assertEqual(trueBool, True)
-        self.assertEqual(falseBool, False)
-
-        db_connection.close
-        db_cursor.close
-    """
-
-    """
-    #UNITTEST: loadDatabase
-    def test_loadDatabase(self):
-        #how can I look at a database and assert that it has stuff? 
-        #I'll prolly copy what is done in load to check if there's already stuff?
-        #mysql.connector is imported.
-        db_connection, db_cursor = connectDB(self.login_dict)
-
-        #NOTE: I need a pickle w/processed bayview data, just after extract. 
-        #NOTE: I'm going to wait until I can work with someone more familiar with load and such to make this.
-
-        #The basic idea will be to fetch the # of rows in the tables of minute, hourly, and daily, and make sure they
-        #match with what they should be. If that's all good, we call that a successful test. 
-
-        db_connection.close
-        db_cursor.close
-        return
-    """
+        cxn.close()
+        cursor.close()
 
 
 if __name__ == '__main__':
