eb8ff9e6d525400ba626042552eae9910e8c33d6
caseymang <caseymang@yahoo.com>
2023-02-27 15:25:37 -0800

    Updated rename_sensors to get variable names for specific site

--- src/ecotope_package_cs2306/transform.py
+++ src/ecotope_package_cs2306/transform.py
@@ -49,6 +49,8 @@ def rename_sensors(df : pd.DataFrame, variable_names_path: str = f"{_input_direc
     
     variable_data = variable_data[1:86]
     variable_data = variable_data[['variable_alias', 'variable_name']]
+    if (site != ""):
+        variable_data = variable_data.loc[variable_data['site'] == site]
     variable_data.dropna(axis=0, inplace=True)
     variable_alias = list(variable_data["variable_alias"])
     variable_true = list(variable_data["variable_name"])
