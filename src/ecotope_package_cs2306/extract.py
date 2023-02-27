eadde80ee2537627884b92337c6134cc79b64665
caseymang <caseymang@yahoo.com>
2023-02-26 21:06:19 -0800

    Updated extract_files()

--- src/ecotope_package_cs2306/extract.py
+++ src/ecotope_package_cs2306/extract.py
@@ -82,8 +82,7 @@ def extract_files(extension : str, subdir : str = "") -> List[str]:
     Input: File extension as string
     Output: List of filenames 
     """
-    os.chdir(os.getcwd())
-    print(os.getcwd())
+    os.chdir(os.path.join(os.getcwd(), "/data"))
     filenames = []
     for file in os.listdir(os.path.join(_data_directory, subdir)):
       if file.endswith(extension):
