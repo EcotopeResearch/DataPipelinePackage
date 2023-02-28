50d7f0e6dc9f11eaf486e94a48365b8a1b25c424
Carlos Bello <bellocarlos@seattleu.edu>
2023-02-28 00:38:33 -0800

    Added condensate calculations

--- src/ecotope_package_cs2306/lbnl.py
+++ src/ecotope_package_cs2306/lbnl.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+from ecotope_package_cs2306.config import configure
 
 def site_specific(df : pd.DataFrame, site : str) -> pd.DataFrame:
     """
@@ -29,6 +30,7 @@ def site_specific(df : pd.DataFrame, site : str) -> pd.DataFrame:
     
     return df
 
+<<<<<<< Updated upstream
 def lbnl_sat_calculations(df: pd.DataFrame) -> pd.DataFrame:
     df_temp = df.filter(regex=r'.*Temp_SAT.*')
     df["Temp_SATAvg"] = df.mean(axis=1)
@@ -43,3 +45,36 @@ def lbnl_pressure_conversions(df: pd.DataFrame) -> pd.DataFrame:
 
     else:
         return df
+=======
+
+def condensate_calculations(df: pd.DataFrame, site: str) -> pd.DataFrame:
+    site_info_directory = configure.get('site_info', 'directory')
+    site_info = pd.read_csv(site_info_directory)
+    oz_2_m3 = 1 / 33810  # [m3/oz]
+    water_density = 997  # [kg/m³]
+    water_latent_vaporization = 2264.705  # [kJ/kg]
+
+    # Condensate calculations
+    if "Condensate_ontime" in df.columns:
+        cycle_length = site_info.loc[site_info["site"]
+                                     == site, "condensate_cycle_length"].iloc[0]
+        oz_per_tip = site_info.loc[site_info["site"]
+                                   == site, "condensate_oz_per_tip"].iloc[0]
+
+        df["Condensate_oz"] = df["Condensate_ontime"].diff().shift(-1).apply(
+            lambda x: x / cycle_length * oz_per_tip if x else x)
+    elif "Condensate_pulse_avg" in df.columns:
+        oz_per_tip = site_info.loc[site_info["site"]
+                                   == site, "condensate_oz_per_tip"].iloc[0]
+
+        df["Condensate_oz"] = df["Condensate_pulse_avg"].apply(
+            lambda x: x * oz_per_tip)
+
+    # Get instantaneous energy from condensation
+    if "Condensate_oz" in df.columns:
+        df["Condensate_kJ"] = df["Condensate_oz"].apply(
+            lambda x: x * oz_2_m3 * water_density * water_latent_vaporization / 1000)
+        df = df.drop(columns=["Condensate_oz"])
+
+    return df
+>>>>>>> Stashed changes
