6080a0a17bf7f900eaf98a230dfacf692c480a9b
Julian Harris <harrisjulian@seattleu.edu>
2023-03-02 14:22:45 -0800

    Updated transform_test.py

--- testing/LBNL/transform/transform_test.py
+++ testing/LBNL/transform/transform_test.py
@@ -5,6 +5,9 @@ from ecotope_package_cs2306 import something#functions used in LBNL transform
 class Test_Transform(unittest.TestCase):
     #class data (maybe, var_names directory)
 
+    #NOTE: Instead of checking if files are missing inside functions, we instead do that at the start? 
+    #Check that test function instead of all of those
+
     #Carlos
     def test_create_fan_curves(self):
         pass
@@ -18,7 +21,10 @@ class Test_Transform(unittest.TestCase):
         pass
 
     #Julian 
-    def test_fridge_placeholder(self):
+    def test_fridge_placeholder_valid(self):
+        #test that the files you need have the proper variables
+
+        #test that Refrig_charge gets modified in the dataframe
         pass
 
 if __name__ == '__main__':
