1a068ca40f11cfed17ac614d9208c5c57e532802
Carlos Bello <bellocarlos@seattleu.edu>
2023-04-18 22:05:55 -0700

    bug fix fan coefficients

--- src/ecotope_package_cs2306/lbnl.py
+++ src/ecotope_package_cs2306/lbnl.py
@@ -595,12 +595,14 @@ def create_fan_curves(cfm_info: pd.DataFrame, site_info: pd.Series) -> pd.DataFr
         """
         X = group[['ID_blower_rms_watts']].values ** 0.3333 - 1
         y = group['ID_blower_cfm'].values
-        return pd.Series(LinearRegression().fit(X, y).coef_)
-    print("HERE")
+
+        # For the linear regression model generated below, return the a and b coefficients
+        fan_model = LinearRegression().fit(X, y)
+        a, b = fan_model.intercept_, fan_model.coef_
+        coefficients = pd.Series({'a': a, 'b': b[0]})
+        return coefficients
     fan_coeffs = by_site.apply(estimate_coefficients)
-    print(fan_coeffs)
     fan_coeffs = fan_coeffs.reset_index()
-    fan_coeffs.columns = ['a', 'b']
     return fan_coeffs
 
 
